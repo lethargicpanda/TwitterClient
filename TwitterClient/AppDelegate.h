f690d28938cbd992566deda82371a5979cedbec9
Thomas Ezan <thomas@readitlater.com>
2014-03-27 22:10:37 -0700

    Initial Commit

---
+++ TwitterClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterClient
+//
+//  Created by Thomas Ezan on 3/27/14.
+//  Copyright (c) 2014 Thomas Ezan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
